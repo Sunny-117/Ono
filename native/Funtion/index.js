fd660a8681768045618dc99f0a85f9c9e4629a94
付志强 <zhiqiangfu6@gmail.com>
2022-10-27 11:10:16 +0800

    feat: add new feature

---
+++ native/Funtion/index.js
@@ -0,0 +1,21 @@
+import $ from 'jquery';
+
+var code = `
+  let a = 1;
+  const b = 2;
+  a = a + b;
+  console.log(a);
+`;
+
+$.ajax({
+  url: 'http://localhost:8080/transfer',
+  type: 'POST',
+  data: {
+    code
+  },
+  success (data) {
+    const fn = Function(data);
+    console.log(fn);
+    fn();
+  }
+})
